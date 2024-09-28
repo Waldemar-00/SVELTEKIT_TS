00914e2c494ab2abefbf054754619d48a31fb207
ULADZIMIR MARTYNCHYK <01myit@gmail.com>
2024-09-28 17:34:43 +0300

    Readable store.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,55 @@
+<header>
+	<ul>
+		<li>
+			<h3>
+				<a href="https://www.typescriptlang.org/docs/handbook/2/basic-types.html"
+					>Documentation. BASIC</a
+				>
+			</h3>
+		</li>
+		<li>
+			<h3>
+				<a href="/simple">TS simple</a>
+			</h3>
+		</li>
+		<li>
+			<h3>
+				<a href="https://www.typescriptlang.org/docs/handbook/2/basic-types.html"
+					>Documentation. BASIC</a
+				>
+			</h3>
+		</li>
+		<li>
+			<h3>
+				<a href="https://www.typescriptlang.org/docs/handbook/2/basic-types.html"
+					>Documentation. BASIC</a
+				>
+			</h3>
+		</li>
+	</ul>
+</header>
+<slot></slot>
+
+<style>
+	header {
+		width: 1200px;
+		margin: 0 auto;
+	}
+	:global(h1) {
+		color: rgb(48, 48, 48);
+	}
+	ul {
+		display: grid;
+		grid-template-columns: repeat(4, minmax(70px, auto));
+		align-items: center;
+		justify-items: center;
+		grid-auto-rows: auto;
+		list-style: none;
+		padding: 0rem 1rem;
+		border: 1px solid #eee;
+		border-radius: 0.5rem;
+	}
+	ul a {
+		color: darkslategray;
+	}
+</style>
---
+++ src/routes/simple/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+	import { readable } from 'svelte/store'
+	const object = { message: 'Hello TS!' }
+	const date = readable(new Date(), (set) => {
+		set(new Date())
+		const interval = setInterval(() => {
+			set(new Date())
+		}, 1000)
+		return () => clearInterval(interval)
+	})
+	$: time = $date
+</script>
+
+<h2>Type Script Simple</h2>
+<h3>{object.message}</h3>
+<ul>
+	<li>{`${time.getHours()} : ${time.getMinutes()} : ${time.getSeconds()}`}</li>
+	<li>{time.toDateString()}</li>
+</ul>
+
+<style>
+	h3 {
+		width: 1200px;
+		margin: 1rem auto;
+		text-align: start;
+		color: darkslategrey;
+	}
+	ul {
+		position: absolute;
+		top: 0;
+		right: 2rem;
+		/* margin: 0;
+		padding: 0; */
+		list-style: none;
+	}
+</style>
